533dc1079d62c0104ec98dc42db26574092e829c
pncalbl <58671677+pncalbl@users.noreply.github.com>
2021-08-22 22:03:29 +0800

    整合配置
    将跨域配置和 Gson 配置，整合进 Web 配置，方便管理。

--- src/main/java/com/pncalbl/sportplay/config/WebConfig.java
+++ src/main/java/com/pncalbl/sportplay/config/WebConfig.java
@@ -1,6 +1,13 @@
 package com.pncalbl.sportplay.config;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author pncalbl
@@ -9,5 +16,38 @@ package com.pncalbl.sportplay.config;
  * @description
  **/
 
-public class WebConfig  {
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	/**
+	 * Gson 配置
+	 *
+	 * @return 消息转换器
+	 */
+	@Bean
+	GsonHttpMessageConverter gsonHttpMessageConverter() {
+		GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+		GsonBuilder gb = new GsonBuilder();
+		Gson gson = gb.create();
+		converter.setGson(gson);
+		return converter;
+	}
+
+	/**
+	 * 配置跨域请求
+	 *
+	 * @param registry 跨域策略
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				// 允许的来源, * 代表允许所有来源
+				.allowedOrigins("http://localhost:8080", "null")
+				.allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+				// 是否允许发送Cookie
+				.allowCredentials(true)
+				// 本次预检请求的有效期，单位为秒
+				.maxAge(3600)
+				.allowedHeaders("*");
+	}
 }
